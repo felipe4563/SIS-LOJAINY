3cf333ce02f3352645f5ba36ac5a811f54d95e8f
felipe4563 <ruben16felipe@gmail.com>
2026-06-06 09:26:58 -0400

    v2

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,6 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", JSON.stringify(process.env.DB_PASSWORD));
-console.log("DB_NAME:", process.env.DB_NAME);
-
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
